4f593273302b181146ec49af412fb45a81442674
Luiz <luizfranzon05@hotmail.com>
2022-03-15 15:35:45 -0300

    Tipos de dados em JS

---
+++ exercicios/10-guia-estelar-javascript/scripts/02-tipos-de-dados.js
@@ -0,0 +1,44 @@
+//Tipos de dados em JavaScript
+
+//Strings
+console.log("String com aspas duplas")
+console.log('String com aspas simples')
+console.log(`String com crases que suporta
+multi-linha`)
+
+console.log('-------------------------------')
+
+//Números
+console.log(33)         //Integer/Inteiro
+console.log(48.15)      //Float
+console.log(12.5 / "a") //NaN - Not a number
+
+console.log('-------------------------------')
+
+//Boolean
+console.log(true)   //Verdadeira
+console.log(false)  //Falso
+
+console.log('-------------------------------')
+
+//Null e Undefined
+console.log(null, undefined)
+console.log(null === undefined)
+
+console.log('-------------------------------')
+
+//Object 
+console.log({
+    nome: "Felipe",
+    idade: 20,
+    andar: function() {
+        console.log('Andei')
+    }
+})
+
+console.log('-------------------------------')
+
+//Arrays
+console.log(["Leite","Ovos","Banana"])
+
+console.log('-------------------------------')
