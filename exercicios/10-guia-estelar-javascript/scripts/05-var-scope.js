24b0e85b66f49c8ac0c62534af5dcca91674e5de
Luiz <luizfranzon05@hotmail.com>
2022-03-15 16:48:56 -0300

    var scope

---
+++ exercicios/10-guia-estelar-javascript/scripts/05-var-scope.js
@@ -0,0 +1,22 @@
+//Scope
+
+//Block Statement
+
+{
+    var x = "Luiz"
+    console.log("Dentro do Block: " + x)
+}
+
+console.log("Fora do block: "+ x) //Uncaught ReferenceError
+
+/*
+a variavel let = x só é visivel 
+dentro do block, pois usa a declaração let
+*/
+
+console.log(dale)
+
+{
+    var dale = 0
+}
+
