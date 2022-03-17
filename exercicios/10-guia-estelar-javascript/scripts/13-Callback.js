15c76f784c3496c81338fdf84fdb010152fd05ca
Luiz <luizfranzon05@hotmail.com>
2022-03-17 20:14:37 -0300

    Callback

--- exercicios/10-guia-estelar-javascript/scripts/13-Callback.js
+++ exercicios/10-guia-estelar-javascript/scripts/13-Callback.js
@@ -1,11 +1,11 @@
 //Uma callback function é uma função que está sendo passada para outra função como parâmetro.
 
 function callBackTest(func) {
-    console.log('1')
-    func()
-    console.log("2")
+    console.log("1");
+    func();
+    console.log("2");
 }
 
-callBackTest(function() {
-    console.log('função dentro de outra função')
-})
+callBackTest(function () {
+    console.log("função dentro de outra função");
+});
