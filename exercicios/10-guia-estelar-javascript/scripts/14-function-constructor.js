0de09a209f0d13f7900a04bb3a66b9e5d6f33e6f
Luiz <luizfranzon05@hotmail.com>
2022-03-17 20:35:09 -0300

    Function constructor

--- exercicios/10-guia-estelar-javascript/scripts/14-function-constructor.js
+++ exercicios/10-guia-estelar-javascript/scripts/14-function-constructor.js
@@ -4,13 +4,13 @@ function Person(name, age) {
     this.name = name;
     this.age = age;
     this.say = function (word) {
-        word = word ? word : "Hi!"
-        return this.name + " falou: " + word
-    }
+        word = word ? word : "Hi!";
+        return this.name + " falou: " + word;
+    };
 }
 
 const luiz = new Person("Luiz", 18);
 const pedro = new Person("Pedro", 21);
 
-console.log(luiz.say('Oie'));
-console.log(pedro.say('Eu não!'))
+console.log(luiz.say("Oie"));
+console.log(pedro.say("Eu não!"));
