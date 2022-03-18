e096a870bb7e2220f9b96ad48c37e4833a77eaba
Luiz <luizfranzon05@hotmail.com>
2022-03-18 14:58:56 -0300

    Type conversion vs Type Coersion

---
+++ exercicios/10-guia-estelar-javascript/scripts/15-typeCoersionEConvertion.js
@@ -0,0 +1,8 @@
+//Type conversion vs Type Coersion ->
+
+let final1 = "10" + 5        //-> Type Coersion
+let final2 = String(10) + 5  //-> Type Conversion
+
+
+console.log(final1)
+console.log(final2)
