38a1a8a24d9880d2afecdfc46a6fc356be7e2483
Luiz <luizfranzon05@hotmail.com>
2022-03-20 16:21:36 -0300

    Contar quanto caracteres/dígitos

---
+++ exercicios/10-guia-estelar-javascript/scripts/17-contar-caracteres_digitos.js
@@ -0,0 +1,7 @@
+// Contar quanto caracteres/digitos
+
+let word = "asdfas123123dfadawddawwaddawawdawwefsehsedfgsef"
+let number = 713838690334234
+
+console.log(word.length)
+console.log(String(number).length)
