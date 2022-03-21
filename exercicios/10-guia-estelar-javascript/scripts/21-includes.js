639a464aac320703edbce524f4f907e974611671
Luiz <luizfranzon05@hotmail.com>
2022-03-20 21:01:00 -0300

    Includes

---
+++ exercicios/10-guia-estelar-javascript/scripts/21-includes.js
@@ -0,0 +1,5 @@
+//Includes
+
+let frase = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget lectus at dolor tristique placerat in a dolor."
+
+console.log(frase.includes('ipsum')) //True
