d0cc77736b7bc4a6ee21fc6b5c2c789c8572a852
Luiz <luizfranzon05@hotmail.com>
2022-03-21 09:00:42 -0300

    .

---
+++ exercicios/10-guia-estelar-javascript/scripts/24-ummonte.js
@@ -0,0 +1,26 @@
+//Manipulando Arrays
+
+let array1 = ["HTML","CSS","JS"]
+
+//Item ao fim da array
+array1.push("NODEJs")
+
+//Adicionar no começo da Array
+array1.unshift("SQL")
+
+//Remover no final
+array1.pop()
+
+//Remover do começo
+array1.shift()
+
+//Pegar somente alguns elementos do array
+// console.log(array1.slice(1,3))
+
+//Remover 1 ou mais elementos de um array
+// array1.splice(1,1)
+
+//Encontrar elemento em uma array
+let index = array1.indexOf("CSS")
+
+console.log(array1)
