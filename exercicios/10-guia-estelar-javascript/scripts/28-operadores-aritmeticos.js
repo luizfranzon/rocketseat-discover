75a8e14ca4d314bc32b63ffe1c5fb1caa91a6613
Luiz <luizfranzon05@hotmail.com>
2022-03-22 15:57:33 -0300

    Operadores Aritméticos

---
+++ exercicios/10-guia-estelar-javascript/scripts/28-operadores-aritmeticos.js
@@ -0,0 +1,19 @@
+//Operadores Aritméticos
+
+//Adição
+console.log(10 + 10)
+
+//Subtração
+console.log(10 - 5)
+
+//Multiplicação
+console.log(4 * 2)
+
+//Divisao
+console.log(180 / 8)
+
+//Resto
+console.log(100 % 15)
+
+//Exponencial
+console.log(2 ** 8) //Elevado
