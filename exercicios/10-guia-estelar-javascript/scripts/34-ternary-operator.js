0e923b0faab3ca5826956f91fccd0b0dbd7753a5
Luiz <luizfranzon05@hotmail.com>
2022-03-23 16:35:46 -0300

    Operador Ternario

---
+++ exercicios/10-guia-estelar-javascript/scripts/34-ternary-operator.js
@@ -0,0 +1,15 @@
+//Operador Ternario
+
+// Condição ? value1 : value2
+
+let pao = true;
+let queijo = true;
+
+const niceBreakfast = pao && queijo ? "Café top" : "Café ruim";
+
+console.log(niceBreakfast);
+
+let age = 18;
+
+const canDrive = age >= 18 ? "Pode dirigir" : "Não pode dirigir";
+console.log(canDrive);
