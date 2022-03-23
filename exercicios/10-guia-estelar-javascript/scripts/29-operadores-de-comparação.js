6642c499563706f817300d569d1cedd46adcfa7c
Luiz <luizfranzon05@hotmail.com>
2022-03-23 14:00:31 -0300

    Operadores de Comparação

---
+++ exercicios/10-guia-estelar-javascript/scripts/29-operadores-de-comparação.js
@@ -0,0 +1,12 @@
+//Operadores de comparação
+
+let one = 1;
+
+console.log(one == 1)   //True
+console.log(one == "1") //True
+console.log(one == 2)   //False
+
+console.log(one != 1)   //False
+console.log(one != "1") //True
+console.log(one != 2)   //True
+
