431c79571b0d57eab168da75999306337ae8a86c
Luiz <luizfranzon05@hotmail.com>
2022-03-24 15:28:24 -0300

    switch

---
+++ exercicios/10-guia-estelar-javascript/scripts/37-switch.js
@@ -0,0 +1,40 @@
+//Switch
+
+// let expression = 'a'
+
+// switch (expression) {
+//     case 'a':
+//         console.log('a')
+//         break
+//     case 'b':
+//         console.log('b')
+//         break
+//     default:
+//         console.log("default")
+// }
+
+//Calculadora em Switch
+
+function calculate(number1, operator, number2) {
+    let result;
+    switch (operator) {
+        case "+":
+            result = number1 + number2;
+            break;
+        case "-":
+            result = number1 - number2;
+            break;
+        case "*":
+            result = number1 * number2;
+            break;
+        case "/":
+            result = number1 / number2;
+            break;
+        default:
+            result = "não implementado";
+            break;
+    }
+    return result;
+}
+
+console.log(calculate(1, "+", 3));
