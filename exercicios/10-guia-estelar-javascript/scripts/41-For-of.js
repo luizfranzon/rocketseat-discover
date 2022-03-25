d9dcdcf8aebf87e21d25dca1eb0c6c8c8aea36cc
Luiz <luizfranzon05@hotmail.com>
2022-03-25 14:05:56 -0300

    For of

---
+++ exercicios/10-guia-estelar-javascript/scripts/41-For-of.js
@@ -0,0 +1,13 @@
+//For of
+
+let name = "Luiz";
+let names = ["Abner", "Bianca", "Carlos"];
+
+for (let char of name) {
+    console.log(char);
+}
+
+for (let name of names) {
+    console.log(name);
+}
+
