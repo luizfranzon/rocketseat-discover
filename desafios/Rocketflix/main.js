773f9b4f7abcfb0ec1818266b3d38401743fec7a
Luiz <luizfranzon05@hotmail.com>
2022-04-18 14:34:57 -0300

    Rocketflix style

---
+++ desafios/Rocketflix/main.js
@@ -0,0 +1,11 @@
+import {
+  API_KEY, BASE_URL,
+  IMG_URL,
+  language,
+} from './api.js'
+
+const movieImg = document.getElementById("movieImg")
+const movieDesc= document.getElementById("movieDesc")
+
+console.log(movieDesc)
+console.log(movieImg)
