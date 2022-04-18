924fec05d6c6f2836c1c03dedb4c8fcff50a9e44
Luiz <luizfranzon05@hotmail.com>
2022-04-18 17:05:38 -0300

    Rocketflix finalizado!

--- desafios/Rocketflix/main.js
+++ desafios/Rocketflix/main.js
@@ -1,11 +1,31 @@
-import {
-  API_KEY, BASE_URL,
-  IMG_URL,
-  language,
-} from './api.js'
+import { API_KEY, language, IMG_URL } from "./api.js"
+
+let randomID;
+const apiURL = `https://api.themoviedb.org/3/movie/`
 
 const movieImg = document.getElementById("movieImg")
-const movieDesc= document.getElementById("movieDesc")
+const movieDesc = document.getElementById("movieDesc")
+const movieTitle = document.getElementById("movieTitle")
+const button = document.getElementById("button")
 
-console.log(movieDesc)
-console.log(movieImg)
+function getMovieData() {
+    let randomID = Math.floor(Math.random() * 99999)
+    axios.get(`${apiURL}${randomID}?${API_KEY}${language}`)
+    .then(response => {
+        const data = response.data
+        let status_code = response.status
+        
+
+        movieTitle.innerText = data.title
+        movieDesc.innerText = data.overview
+        movieImg.src = `${IMG_URL}${data.poster_path}`
+        // console.log(apiURL)
+    })
+    .catch(error => {
+        console.log(error)
+    })
+}
+
+button.addEventListener('click', getMovieData)
+
+
