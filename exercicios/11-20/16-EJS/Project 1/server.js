9888083d2e6ed55b42d5fd88ed358524603a043e
Luiz <luizfranzon05@hotmail.com>
2022-04-26 13:28:08 -0300

    server

--- exercicios/11-20/16-EJS/Project 1/server.js
+++ exercicios/11-20/16-EJS/Project 1/server.js
@@ -4,11 +4,48 @@ const app = express();
 app.set("view engine", "ejs");
 
 app.get("/", function (req, res) {
-    res.render("pages/index");
+    const items = [
+        {
+            title: "D",
+            message: "esenvolver de forma rapida",
+        },
+        {
+            title: "E",
+            message: "JS usa JavaScript para renderizar HTML.",
+        },
+        {
+            title: "M",
+            message: "uito fácil de usar.",
+        },
+        {
+            title: "A",
+            message: "gil",
+        },
+        {
+            title: "I",
+            message: "Ncrivel",
+        },
+        {
+            title: "S",
+            message: "uper eficiente",
+        },
+    ];
+
+    const subtitle = "EJS";
+
+    res.render("pages/index", {
+        qualities: items,
+        subtitle: subtitle,
+    });
+    
 });
 
 app.get("/about", function (req, res) {
     res.render("pages/about");
+});
+
+app.get("*", function (req, res) {
+    res.render("pages/404");
 });
 
 app.listen(8080);
