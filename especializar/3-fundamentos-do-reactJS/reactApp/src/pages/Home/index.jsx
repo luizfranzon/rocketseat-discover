584f55d8e3c600a4cbced86e58d5bbbcb9292e03
Luiz <luizfranzon05@hotmail.com>
2022-06-30 10:52:34 -0300

    added className and changed placeholder text

--- especializar/3-fundamentos-do-reactJS/reactApp/src/pages/Home/index.jsx
+++ especializar/3-fundamentos-do-reactJS/reactApp/src/pages/Home/index.jsx
@@ -1,10 +1,12 @@
+import './styles.css'
+
 function Home() {
 
     return (
-        <div>
+        <div className="container">
             <h1>Lista de Presença</h1>
-            <input type="text" placeholder="nome do usuário"/>
-            <button>Adicionar usuários</button>
+            <input type="text" placeholder="Nome do usuário"/>
+            <button>Adicionar</button>
         </div>
     )
 }
