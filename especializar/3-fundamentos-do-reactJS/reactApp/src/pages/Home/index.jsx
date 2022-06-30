7ff249879c68675fb52cfe3b4262d4fbbc9b20aa
Luiz <luizfranzon05@hotmail.com>
2022-06-30 14:56:37 -0300

    added function to get time and ...prevState

--- especializar/3-fundamentos-do-reactJS/reactApp/src/pages/Home/index.jsx
+++ especializar/3-fundamentos-do-reactJS/reactApp/src/pages/Home/index.jsx
@@ -4,23 +4,37 @@ import './styles.css'
 import Card from "../../components/Card"
 
 function Home() {
-    const [studentName, setStudentName] = useState("Luiz");
+    const [studentName, setStudentName] = useState("Nome não definido");
+    const [students, setStudents] = useState([])
+
+    function handleAddStudent() {
+        const newStudent = {
+            name: studentName,
+            time: new Date().toLocaleTimeString("pt-br", {
+                hour: '2-digit',
+                minute: '2-digit',
+                second: '2-digit'
+            })
+        };
+
+        setStudents(prevState => [...prevState, newStudent])
+    }
+
     return (
         <div className="container">
 
             <h1>Lista de Presença</h1>
-
-            <span>Nome: {studentName}</span>
 
             <input 
                 type="text" 
                 placeholder="Nome do usuário"
                 onChange={e => setStudentName(e.target.value)}
             />
-            <button>Adicionar</button>
+            <button onClick={handleAddStudent}>Adicionar</button>
 
-            <Card name="Luiz" time="10:05:40"/>
-            <Card name="Daniel" time="11:12:47"/>
+            {
+                students.map(student => <Card name={student.name} time={student.time}/>)
+            }
 
         </div>
     )
